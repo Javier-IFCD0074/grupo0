ce04bd7f0b62d253472148812adfc4309aeab184
jotcurso1 <ivan.dawm01@gmail.com>
2024-12-20 14:11:28 +0100

    queda pendiente la solucion para el dia lunes

--- asterix.js
+++ asterix.js
@@ -1,6 +1,6 @@
 //document.getElementById("mostrar").addEventListener("click", mostrarCosas, true);
-//document.getElementById("mostrar").addEventListener("click", mostrarDatosAPI, true);
-document.getElementById("mostrar").addEventListener("click", crear_tabla, true);
+document.getElementById("mostrar").addEventListener("click", mostrarDatosAPI, true);
+//document.getElementById("mostrar").addEventListener("click", crear_tablav1, true);
 document.getElementById("limpiar").addEventListener("click", limpiarDatosAPI, true);
 
 let contenido = document.querySelector("#contenido");
@@ -9,7 +9,7 @@ function mostrarDatosAPI()
 {
 //contenido.innerHTML = 'Hola Mundo';
 console.log('Ha pulsado mostrar usuario');
-fetch("https://randomuser.me/api/")
+fetch("https://randomuser.me/api?results=5")
     .then(res => res.json())
     .then(data =>{
             console.log(data);
@@ -18,6 +18,9 @@ fetch("https://randomuser.me/api/")
             contenido.innerHTML = `${persona.title} `;
             contenido.innerHTML += ` ${persona.first} `;
             contenido.innerHTML += ` ${persona.last} <br>`;
+
+        for (i=0; i<5 ; i++)
+            contenido.innerHTML+=`${data.results[i].name.first}<br>`;
 
             let url_foto = data.results [0].picture.large;
             console.log(url_foto);
@@ -38,7 +41,7 @@ function mostrarCosas()
             contenido.innerHTML += ` Colegas ${i} <br>`;
     }
 }
-function crear_tabla()
+function crear_tablav0()
 {
     contenido.innerHTML = 'tabla:<br>' 
     for(i=0; i<=5 ; i ++)    
@@ -50,6 +53,35 @@ function crear_tabla()
             contenido.innerHTML += ` -celda ${j} `;
         contenido.innerHTML += `<br>`;
         }
+
+}
+
+
+function mostrarCosas()
+{
+    contenido.innerHTML = 'Hola '
+    for(i=0; i<5; i ++)
+    {   
+        if(i==2)
+            contenido.innerHTML += ' Perros <br>';
+        else
+            contenido.innerHTML += ` Colegas ${i} <br>`;
+    }
+}
+function crear_tablav1()
+{
+    contenido.innerHTML = '<table border=1>' 
+    for(i=0; i<5; i ++)    
+    {
+        
+            contenido.innerHTML += `<tr>`; //` fila ${i} <br>`;//empieza fila
+
+            for(j=0; j<3 ; j++)            
+            contenido.innerHTML += ` <td> celda ${i}-${j} </td>`;
+        contenido.innerHTML += `</tr>`;//`<br>`; //final de fila
+        }
+
+        contenido.innerHTML += `</table>`;
 
 }
 

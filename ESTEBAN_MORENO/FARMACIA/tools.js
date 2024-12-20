d70cb1b1492527b44b643915055e886e4596ff99
3msDvlp <ems.dvlp.dmz@gmail.com>
2024-12-20 13:08:57 +0100

    UPD EMS MULTIPLE RELEASE 0 - NO TIENE DATOS REALES DE USUARIO. SOLO LA ESTRUCTURA DE TABLA -.

--- ESTEBAN_MORENO/FARMACIA/tools.js
+++ ESTEBAN_MORENO/FARMACIA/tools.js
@@ -1,14 +1,18 @@
-//document.getElementById("mostrar").addEventListener("click", mostrarCosas, true);
-document.getElementById("numreg").addEventListener("click", mostrarDatosAPI, true);
+document.getElementById("numreg").addEventListener("click", MostrarDatosAPI, true);
 console.log(numreg);
 document.getElementById("limpiar").addEventListener("click", LimpiarDatosForm, true);
-
 let contenido = document.querySelector("#contenido");
 
-function mostrarDatosAPI()
+console.log(numreg);
+contenido.innerHTML = `${numreg}`
+
+function MostrarDatosAPI()
 {
 console.log('Ha pulsado medicamento. Vamos a buscarlo ...');
-fetch("https://cima.aemps.es/cima/rest/medicamento?nregistro=51347")
+
+let URLfetchAPI = "https://cima.aemps.es/cima/rest/medicamento?nregistro=" & `$(numreg)`;
+console.log(URLfetchAPI)
+fetch(URLfetchAPI)
     .then(res => res.json())
     .then(data => { 
             console.log(data);
@@ -16,32 +20,18 @@ fetch("https://cima.aemps.es/cima/rest/medicamento?nregistro=51347")
             console.log(medicamento);
             contenido.innerHTML = `${medicamento.nombre} `;
             contenido.innerHTML += ` ${medicamento.nregistro} `;
-            contenido.innerHTML += ` ${medicamento.labtitular} <br>`;
-
+            contenido.innerHTML +=` ${medicamento.labtitular} <br>`;
+                       
             let url_foto = data.fotos[0].url;
             console.log(url_foto);
-            let imagen= `<img src="${url_foto}" alt="Mi ejemplo">`;
-            contenido.innerHTML += `${imagen}<br>`;
+            let imagen= `<img src="${url_foto}" alt="Mi ejemplo">`;          
+            
 }
     )
 }
    
-function mostrarCosas()
-{
-    contenido.innerHTML = 'Hola '
-    for(i=0; i<7; i ++)
-    {  
-        if(i==2)
-            contenido.innerHTML += ' Perros <br>';
-        else
-            contenido.innerHTML += ` Colegas ${i} <br>`;
-    }
-}
-
 function LimpiarDatosForm()
 {
-    console.log('Ha pulsado el boton de limpiar datos del form');
-    Codigo.innerHTML = '';
-    
-    //contenido.innerHTML = '';
+    console.log('He pulsado el boton de limpiar datos del form');
+    contenido.innerHTML = '';
 }

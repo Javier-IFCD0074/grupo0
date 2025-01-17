15378573506db366a4cdd55354c3614b33d80e2a
Javier Salazar <javier.salazar.cic@gmail.com>
2025-01-17 19:54:20 +0100

    Codigo revisado y comentado

---
+++ 20250117/api.bat
@@ -0,0 +1,10 @@
+@echo off
+rem ****************
+rem * SERVIDOR API *
+rem * 17 Dic. 2025 *
+rem ****************
+color 57
+title Servidor API (puerto 3000)
+echo Este script se llama api.bat
+echo ============================
+node --watch api_250117.js 
---
+++ 20250117/web.bat
@@ -0,0 +1,10 @@
+@echo off
+rem ****************
+rem * SERVIDOR WEB *
+rem * 17 Dic. 2025 *
+rem ****************
+color A0
+title Servidor WEB (puerto 8000)
+echo Este script se llama web.bat
+echo ============================
+py -m http.server
